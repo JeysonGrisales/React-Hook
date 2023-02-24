dbe185df8888539f2ce7cba9b7cb9c867c17efe2
Jeyson Grisales <grisalesjeyson@gmail.com>
2023-02-24 17:15:04 -0500

    Terminada logica, Terminar de estilizar

--- myfirstproject/src/components/pages/RickAndMorty/RickAndMorty.jsx
+++ myfirstproject/src/components/pages/RickAndMorty/RickAndMorty.jsx
@@ -1,42 +1,63 @@
 import React from 'react'
-import { useState, useRef } from 'react'
+import { useState } from 'react'
+const archivar = []
 
 export const RickAndMorty = () => {
 
   const [text, newText] = useState()
-  const contador = useRef(520)
+  const [inicio, fin] = useState("255")
+  const [oneTwett , newTwett] = useState("")
 
+  // publicar
+  const obtener = () =>{
+    const nameInptu = document.getElementById("textEnterUse").value
+    newText(nameInptu)
+  }
+
+  //Archivar
+  const archivarTwits= () =>{
+    archivar.push(text)
+    localStorage.setItem("twitter", JSON.stringify(archivar))
+    console.log(archivar);
+  }
+
+  //contador
+  const counter = ()=>{
+    let x = document.getElementById("textEnterUse").value
+    fin(255-x.length)
+  }
+
+  // Mostrar Archivados
+
+  const mostrar = ()=>{
+
+    let mostrartwetts = ""
+
+    archivar.map((i) =>{
+      mostrartwetts += "Tweet: " +i 
+      newTwett(mostrartwetts)
+    })
+    
+  }
 
   return (
     <div className='RickAndMorty'>
-      <div className="body">
-        <h1 >Generate of tweets</h1>
-        <div className="containerInfo">
-          <h1 className='titleTwitters'>public of tweet</h1>
-          <textarea onKeyUp={Event} name="" id="textEnterUse" cols="30" rows="10" className='TextEnter' placeholder='write a tweet (maximum 255 characters)'></textarea>
-          <div className="conterCharatcter">  </div>
-          <div className="buttons">
-            <button className='btnInteract' onClick={()=>{
-            const nameInptu = document.getElementById("textEnterUse").value
-            newText(nameInptu)
-            if(nameInptu === ""){
-              newText("Ingrese un dato valido")
-            }
-        }}>Publicar</button>
-            <button className='btnInteract' onClick={()=>{
-              const archivar = []
-              // archivar.push(nameInptu)
-              console.log(archivar);
-            }}
-            >Archivar</button>
-            <button className='btnInteract'>Mostrar Archivados</button>
+        <div className="body">
+          <h1 >Generate of tweets</h1>
+          <div className="containerInfo">
+            <h1 className='titleTwitters'>public of tweet</h1>
+            <textarea name="" id="textEnterUse" cols="30" rows="10" className='TextEnter' placeholder='write a tweet (maximum 255 characters)' onKeyUp={counter}></textarea>
+            <div className="conterCharatcter">{inicio}</div>
+            <div className="buttons">
+              <button className='btnInteract' onClick={obtener}>Publicar</button>
+              <button className='btnInteract' onClick={archivarTwits}>Archivar</button>
+            <button className='btnInteract' onClick={mostrar}>Mostrar Archivados</button>
           </div>
           <h3 id='resultTweet'>{text}</h3>
         </div>
       </div>
           <h4>here you will see your archived tweets</h4>
-          <div className="tweetsArchived">Nan</div>
-          <div className="footer"></div>
+          <div className="tweetsArchived" >{oneTwett}</div>
     </div>
   )
 }
